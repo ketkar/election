49508c9546ff6d34b0dd6433e8134b03b99c7aac
Aniket Ketkar <ketkar@Anikets-MacBook-Pro.local>
2016-04-25 09:46:52 -0700

    Renamed for human-readable columns, example of top features for Sanders

--- util.R
+++ util.R
@@ -5,6 +5,8 @@
 # Use source("util.R") to import these methods. 
 
 library("dplyr")
+library("class") #for use with k nearest neighbors. 
+
 county_features <- read.csv("data/county_facts_dictionary.csv")
 county_data <- read.csv("data/county_facts.csv")
 results <- read.csv("data/primary_results.csv")
@@ -21,9 +23,23 @@ sanders_nums <- subset(sanders, select = - c(state:candidate, fraction_votes:sta
 clinton_nums <- subset(clinton, select = - c(state:candidate, fraction_votes:state_abbreviation.y))
 cruz_nums <- subset(cruz, select = - c(state:candidate, fraction_votes:state_abbreviation.y))
 
+i <- match(names(clinton_nums), county_features$column_name)
+names(trump_nums) <- county_features$description[i]
+names(trump_nums)[[1]] <- "votes"
+names(clinton_nums) <- county_features$description[i]
+names(clinton_nums)[[1]] <- "votes"
+names(cruz_nums) <- county_features$description[i]
+names(cruz_nums)[[1]] <- "votes"
+names(sanders_nums) <- county_features$description[i]
+names(sanders_nums)[[1]] <- "votes"
 
 trump_model <- lm(votes ~ ., trump_nums)
 sanders_model <- lm(votes ~ ., sanders_nums)
 clinton_model <- lm(votes ~ ., clinton_nums)
 cruz_model <- lm(votes ~ ., cruz_nums)
+
+#Example: Top weighted features for Sanders: 
+head(sort(abs(sanders_model$coefficients), decreasing = T), 10)
+#Least important features: 
+head(sort(abs(sanders_model$coefficients), decreasing = F), 10)
 
