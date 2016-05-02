63632cd932ef494e84baa97531c61af6f3efcdb8
Aniket Ketkar <ketkar@Anikets-MacBook-Pro.local>
2016-05-01 23:38:34 -0700

    Training on Idaho

--- knn.R
+++ knn.R
@@ -5,15 +5,12 @@
 source("util.R")
 library("FNN")
 
-
 #Cleaning some more, with labels for win/losses relative to each other. 
 #Assumes two people races 
 trump_wins <- as.numeric(trump_nums$votes - cruz_nums$votes > 0)
 sanders_wins <- as.numeric(sanders_nums$votes - clinton_nums$votes > 0)
 
 # DEPRECATED out <- knn(subset(train, select = -c(votes)), subset(test, select = -c(votes)), train$votes, k = 2)
-
-
 
 #Rewriting K Nearest Neighbors 
 calculate_distance <- function(vec1, vec2){
@@ -26,9 +23,6 @@ knn <- function(cand, data, num = 5){
   return(sort.int(distances, index.return = T)$ix[1:num])
 }
 
-#t <- knn(trump_nums[1, ], trump_nums, 10)
-
-
 #Time for Cross validation! 
 #Partitioning data 
 shuffled_indexes <- sample.int(nrow(trump_nums))
@@ -40,13 +34,16 @@ test <- shuffled[1501:nrow(shuffled), ]
 test_labels <- trump_wins[test_indexes]
 train_labels <- trump_wins[train_indexes]
 
-
-
 #Testing 
-trump_cutdown <- select()
+trump_idaho_train <- trump_nums[which(trump$state == "Idaho"),]
+trump_idaho_labels <- trump_wins[which(trump$state == "Idaho")]
+trump_train <- trump_nums[which(trump$state != "Idaho"), ]
+trump_train_labels <- trump_wins[which(trump$state != "Idaho")]
+
 trump_confidence <- numeric()
-for (i in 1:nrow(trump_nums[1:10,])){
-  trump_confidence[[i]] <- mean(trump_wins[knn(trump_nums[i, ], trump_nums[66:nrow(trump_nums),], 10)[2:10]])
+for (i in 1:nrow(trump_idaho_train)){
+  trump_confidence[[i]] <- mean(trump_train_labels[knn(trump_idaho_train[i, ], trump_train, 15)[2:15]])
 }
 
-correct <- 1 - sum(trump_wins[1:length(trump_confidence)] - as.numeric(trump_confidence > 0.5))/length(trump_confidence)
+correct <- 1 - sum(trump_idaho_labels - as.numeric(trump_confidence > 0.5))/length(trump_confidence)
+
