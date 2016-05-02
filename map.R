1db8d030a1e6c73ff0cea555f8c12be5d9d7e516
Aniket Ketkar <ketkar@airbears2-10-142-62-44.airbears2.1918.berkeley.edu>
2016-05-02 09:47:24 -0700

    working map for Idaho

--- map.R
+++ map.R
@@ -1,7 +1,8 @@
-install.packages("maps")
-install.packages("ggplot2")
+#install.packages("maps")
+#install.packages("ggplot2")
 library(ggplot2)
 library(maps)
+source("knn.R")
 
 # data is a data frame that has the columns: percentages, candidate, and the region.
 map_data <- function(data, color1, color2, state="ohio") {
@@ -15,4 +16,18 @@ map_data <- function(data, color1, color2, state="ohio") {
   + scale_colour_gradient2(low = muted(color1), mid="white", high = muted(color2), guide="colorbar") 
   + theme_bw()  + labs(fill = "Candidate", title = "Winning Candidate by State", x="", y="")
 }
+
+
+
+library(choroplethr)
+library(mapproj)
+
+trump_confidence <- knn_pred(trump_nums, trump_wins, trump, k=20)
+
+trump_confidence$value = trump_confidence$confidence
+trump_confidence$region <- trump[which(trump$state == "Idaho"),]$fips
+county_choropleth(trump_confidence, 
+                  state_zoom = "idaho",
+                  title      = "Trump Win Probability",
+                  num_colors = 9, color = "red")
 
